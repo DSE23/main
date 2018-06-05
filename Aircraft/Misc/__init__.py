3ffa25ca2016c959b9ee53a5034e97a9222229ae
midasgossye <midasgossye@hotmail.com>
2018-06-05 16:12:20 +0200

    Updated comments

--- Aircraft/Misc/__init__.py
+++ Aircraft/Misc/__init__.py
@@ -1,6 +1,8 @@
 """
-DUMMY FILE
+IMPORTANT FILE
 DO NOT EDIT/DELETE
+SEE THE FOLLOWING LINK WHY THIS NEEDS TO BE HERE:
+    https://pint.readthedocs.io/en/latest/tutorial.html
 """
 from pint import UnitRegistry
 ureg = UnitRegistry()
