a030bdae291d4fbf6b7ea0e3187865618f95adb1
midasgossye <midasgossye@hotmail.com>
2018-06-06 09:35:52 +0200

    Added pint inclusion

--- Aircraft/Geometry/Wing.py
+++ Aircraft/Geometry/Wing.py
@@ -3,16 +3,18 @@ Name: Wing
 Department: Geometry
 Last updated: 06/06/2018 12:38 by Boris 
 """
+import sys
+sys.path.append('../') # This makes sure the parent directory gets added to the system path
+
+from Misc import ureg, Q_ # Imports the unit registry fron the Misc folder
 
 A = 5.5                         #Estimate aspect ratio
 t = 0.4                         #Estimate taper
-s = 8.03                        #Estimate span (m)
-s *= ureg.meter                 #giving span meter as unit
+s = Q_("8.03 m")                        #Estimate span (m)
 Lambda25 = 0                    #Quarter chord sweep
 CtoT = 0.15                     #Chord to thickness ratio
 Spar2R = 0.18                     #Ratio of aileron at the root (postion of second spar)
 Spar2T = 0.33                     #Ratio of aileron at the tip (postiion fo second spar)
 Spar1R= 0.15                   #Ratio of LE HLD at root (postion of first spar)
 Spar1T= 0.15                   #Ratio of LE HLD at tip (postion of first spar)
-ChordR = 2.015                  #Length of root (m)
-ChordR *= ureg.meter            #giving root chord meter as unit
+ChordR = Q_("2.015 m")         #Length of root (m)
