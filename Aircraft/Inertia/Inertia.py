e434cdca33d4340a6ad712f546edc7f379fcb046
midasgossye <midasgossye@hotmail.com>
2018-06-06 16:41:49 +0200

    Fixed mistakes

--- Aircraft/Inertia/Inertia.py
+++ Aircraft/Inertia/Inertia.py
@@ -17,7 +17,7 @@ def calc_stringer_Inertia(h_str, w_str, t_str):
     # Calculating Area's of stringer components
     A_1 = h_str*t_str
     A_2 = (w_str-t_str)*t_str
-    
+    print(A_2)
     # Calculating Centroid of stringer w.r.t. bottom left corner
     y_bar = (A_1*h_str/2 + A_2*(h_str-0.5*t_str))/(A_1+A_2)
     x_bar = (A_1*0.5*t_str + A_2*(w_str/2 + t_str))/(A_1+A_2)
@@ -25,12 +25,15 @@ def calc_stringer_Inertia(h_str, w_str, t_str):
     print(x_bar)
     
     I_xx = (t_str*h_str**3)/12 # Ixx inertia of vertical part of the stringer
-    I_xx += A_1*h_str**2
-    I_xx += (t_str*(w_str-t_str))*(h_str-0.5*t_str)**2 # Ixx inertia of horizontal part with thin walled approximation
+    I_xx += A_1*(h_str/2)**2
+    I_xx += (t_str**3 * (w_str-t_str))/12  # Ixx inertia of horizontal part with thin walled approximation
+    I_xx += A_2*(h_str+0.5*t_str)**2
+    
     I_xx.ito("m**4")
     
-    I_yy = ((w_str-t_str)**3*t_str)/12 + (t_str+0.5*w_str)**2*A_2 # Iyy Inertia of horizontal part referenced to bottom-left corner
-    I_yy += A_1*(0.5*t_str)**2 # Iyy of vertical part is negligible (thin-walled approximation)
+    I_yy = (((w_str-t_str)**3) * t_str)/12 + A_2 * (0.5*(w_str))**2
+    #I_yy = ((w_str-t_str)**3 * t_str)/3 # Iyy Inertia of horizontal part referenced to bottom-left corner
+    #I_yy += A_1*(0.5*t_str)**2 # Iyy of vertical part is negligible (thin-walled approximation)
     I_yy.ito("m**4")
     
     I_xy = A_1*(h_str/2 - 0)*(t_str/2 - 0)
