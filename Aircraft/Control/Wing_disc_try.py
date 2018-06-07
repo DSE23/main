3f8e9d16a3b4b442575105f30cd181bb59b44c66
Jurian Stougie <jurianstougie@live.nl>
2018-06-07 13:46:55 +0200

    Initial parameter file added, please read the comment, and check it regurlary!!!!

--- Aircraft/Control/Wing_disc_try.py
+++ Aircraft/Control/Wing_disc_try.py
@@ -6,11 +6,12 @@ Created on Thu Jun  7 11:19:31 2018
 """
 
 import sys
+sys.path.append('../')    # This makes sure the parent directory gets added to the system path
+
 import numpy as np
 from Misc import ureg, Q_      # Imports the unit registry fron the Misc folder
 from Geometry import Wing
-
-sys.path.append('../')    # This makes sure the parent directory gets added to the system path
+from Misc import Init_parm as IP
 
 
 b = Wing.s                      # import Span
@@ -27,7 +28,7 @@ def local_chord(z):  # Calculates the chord at location z(distance from center)
 n_of_disc = 20                  # Number of parts wing is discretized
 bloc = b/n_of_disc              # Span of each piece
 half_b = b/2
-S = 0
+V_inf = IP.V_inf
 
 for i in range(n_of_disc):
     b1 = bloc*i - half_b        # Z boundary left
@@ -36,7 +37,4 @@ for i in range(n_of_disc):
     c1 = local_chord(abs(b1))   # Left chord
     c2 = local_chord(abs(b2))   # Right chord
     Sloc = ((c1+c2)/2)*(b2-b1)
-    S = S + Sloc
-print(S)
-
 
---
+++ Aircraft/Misc/Init_parm.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun  7 11:19:31 2018
+
+@author: jurian
+"""
+
+import sys
+sys.path.append('../')    # This makes sure the parent directory gets added to the system path
+
+# This file contains initial parameters found in the Midterm
+# If there is a file where a value of this list is calculated
+# please link to it, to make sure that all other scripts will
+# continue to work, and will use the latest version of the value
+# !!!Check Regurlary!!!
+
