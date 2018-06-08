06ef75fb4454c9779b171d20ac3491be66e06f64
Jurian Stougie <jurianstougie@live.nl>
2018-06-08 12:34:36 +0200

    More Aero :-)

---
+++ Aircraft/Aerodynamics/General.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun  8 11:59:49 2018
+
+@author: jurian
+"""
+
+
+CD_0 = 0.028                            # Zero lift drag coeff
+alpha_0 = 0.0075                        # Cruise AoA
+
