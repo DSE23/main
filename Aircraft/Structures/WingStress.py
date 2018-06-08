84d20f2ff3d6956350bb2296da5603e590b3fc5d
tobiashaegens <tobias.haegens@gmail.com>
2018-06-08 16:47:31 +0200

    added things for Torsion

--- Aircraft/Structures/WingStress.py
+++ Aircraft/Structures/WingStress.py
@@ -14,6 +14,7 @@ import math as m
 from Geometry import Geometry
 import Structures
 from Structures import Inertia
+from Structures import Wing
 
 from Misc import ureg, Q_ # Imports the unit registry fron the Misc folder
 
@@ -30,6 +31,18 @@ spanleft = b - z
 print(av_chord)
 print(spanleft)
 
+
+#Material properties of the chosen material.
+#Current chosen material:
+#Carbon fiber reinforced carbon matrix composite (Vf:50%)
+youngs_modulus = Q_("95 GPa")
+yield_strength = Q_("18 MPa")
+shear_modulus = Q_("36 GPa") #G
+
+
+
+ 
+
 def Normal_stress_due_to_bending(zs, cs, y): # Normal stress due to bending
     denominator_inertia_term = Inertia.Ixx*Inertia.Iyy-Inertia.Ixy**2
     inertia_term_1 = (Inertia.Iyy*y-Inertia.Ixy*cs)/denominator_inertia_term
@@ -37,4 +50,12 @@ def Normal_stress_due_to_bending(zs, cs, y): # Normal stress due to bending
     sigma_zs = Mx*inertia_term_1 + My*inertia_term_2
     return sigma_zs #Gives the normal stress function for a given span zs, and x- and y- coordinate
 
-
+def Pure_torsion(zs):
+    A_cell = Wing.Area_cell()
+    length_skin = Wing.Area/Wing.ThSkin
+    length_spar1 = Wing.airfoilordinate(Wing.ChSpar1)
+    length_spar2 = Wing.airfoilordinate(Wing.ChSpar2)
+    const_tor = T/(2*A_cell**2*shear_modulus) #constant term in twist formula
+    line_int_tor = (length_skin*2/Wing.t_skin+length_spar1/Wing.ThSpar1+length_spar2/Wing.ThSpar2) #result from line integral from torsion formula
+    twist_wb = const_tor*line_int_tor
+    return twist_wb
