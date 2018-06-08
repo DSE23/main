02826cbc7550fd0f7c7eaab89a75a285f48609cd
midasgossye <midasgossye@hotmail.com>
2018-06-08 13:41:52 +0200

    Added description

--- Aircraft/Misc/Extra_param.py
+++ Aircraft/Misc/Extra_param.py
@@ -1,4 +1,4 @@
-
+### This file contains parameters from the extra that might be useful
 import sys
 sys.path.append('../')
 # This makes sure the parent directory gets added to the system path
@@ -7,5 +7,4 @@ from Misc import ureg, Q_
 # Imports the unit registry from the Misc folder
 
 Extra_roll_MOM = Q_("395 ((lbf*s**2)/ft)*(ft**2)")
-print(Extra_roll_MOM.to(ureg("kg*m^2")))
 
