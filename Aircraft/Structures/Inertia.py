a96c24921a32b267756a7bbd177680541b787022
midas <midasgossye@hotmail.com>
2018-06-11 09:32:21 +0200

    Added stiffener location formula

--- Aircraft/Structures/Inertia.py
+++ Aircraft/Structures/Inertia.py
@@ -79,13 +79,11 @@ def get_coord_from_perim(n_st, start_x, end_x, chord_l, dat_file_name="../Airfoi
     perim_spacing = stif_perim / (n_st + 1)
     step = 0.0001
     i = 0
-    print(perim_spacing)
     for x_c in np.arange(start_x, end_x, step):
         perim += np.sqrt((step) ** 2 + (p(x_c + step) - p(x_c)) ** 2)
         if i >= n_st:
             break
         if abs(perim - perim_spacing) < 10e-5:
-            print(perim)
             final_x_coord = np.append(final_x_coord, 0.5 * (x_c + x_c + step))
             final_y_coord = np.append(final_y_coord, 0.5 * (p(x_c) + p(x_c + step)))
             slope_angles = np.append(slope_angles, -np.arctan((p(x_c + step) - p(x_c)) / (step)) + np.pi)
@@ -205,7 +203,8 @@ print('this is', Calc_skin_inertia_Iyy(Wing.ChSpar1, Wing.ChSpar2))
 
 
 # print(calc_stringer_Inertia(Q_("50 mm"),Q_("20 mm"),Q_("2 mm")))
-print(get_coord_from_perim(5, 0.2, 0.6, Q_("7 m")))
+stiff_x_y_coord = get_coord_from_perim(5, 0.2, 0.6, Q_("7 m"))
+print(stiff_x_y_coord)
 
 # function for transforming axis to a rotated version
 # input MMOI I_zz, I_yy, I_zy, and rotation angle rot_angle
