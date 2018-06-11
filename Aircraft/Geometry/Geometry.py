95e31a6659d66f8e3a1f22c2de97cf023a483754
Emma <emmademeester@hotmail.com>
2018-06-11 15:51:06 +0200

    added canopy area number, to be added by Catia guys later on

--- Aircraft/Geometry/Geometry.py
+++ Aircraft/Geometry/Geometry.py
@@ -56,7 +56,7 @@ class Fuselage(object):
     front_A = Q_("0.98 m**2")              # [m^2] Frontal area
     S_wet_f= Q_("14.94 m**2")              # [m^2] Fuselage wetted area
     cabin_w = Q_("0.6 m")               # [m] cabin width (inside)
-
+    A_max_canopy = Q_(" m**2")          # [m^2], coming from catia
 class H_tail(object):
     
     S = Q_("2.629 m**2")                 # [m^2] Horizontal tail surface
