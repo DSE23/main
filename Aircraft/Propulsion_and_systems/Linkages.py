d55ffdedb52b9861d53b990f540795b58c7516d7
Ties <ties.a.nieuwenhuizen@gmail.com>
2018-06-12 15:11:55 +0200

    Added Stick_pedals.py

--- Aircraft/Propulsion_and_systems/Linkages.py
+++ Aircraft/Propulsion_and_systems/Linkages.py
@@ -18,6 +18,7 @@ from Misc import ureg, Q_
 
 from Geometry import Geometry
 import numpy as np
+import Stick_pedals
 
 # Start defining global variables for easy editing from elsewhere
 # For explanations of the variables defined here, see below, where they are given values
@@ -50,26 +51,21 @@ f_s_p_max = f_s_r_max*2  # From 1:2:3 ratio for control forces
 # Maximum rudder force
 f_r_max = f_s_r_max*3  # From 1:2:3 ratio for control forces
 
-# Stick length
-l_stick = Q_("81 cm")  # As defined by Gijs
-
-# Rudder travel
-l_rudder = Q_("30 cm")  # DUMMY
 # End assigning values
 
 
 # Calculate maximum hinge moment in roll
-m_s_r_max = f_s_r_max * l_stick
+m_s_r_max = f_s_r_max * Stick_pedals.l_s
 m_s_r_max.ito(ureg("newton * meter"))
 print(m_s_r_max)
 
 # Calculate maximum hinge moment in pitch
-m_s_p_max = f_s_p_max * l_stick
+m_s_p_max = f_s_p_max * Stick_pedals.l_s
 m_s_p_max.ito(ureg("newton * meter"))
 print(m_s_p_max)
 
 # Calculate maximum hinge moment in yaw
-r_rudder = l_rudder/np.tan(Geometry.V_tail.delta_r)
+r_rudder = Stick_pedals.d_p/np.tan(Geometry.V_tail.delta_r)
 m_r_max = f_r_max * r_rudder
 m_r_max.ito(ureg("newton * meter"))
 print(m_r_max)
