5528e483da96fee70ffb521fca8eb8ce39934c26
Jordy van Leeuwen <j.vanleeuwen10@gmail.com>
2018-06-12 08:53:46 +0200

    Roll iterated for all speeds between Vs and Va

--- Aircraft/Geometry/Geometry.py
+++ Aircraft/Geometry/Geometry.py
@@ -90,7 +90,7 @@ class V_tail(object):
     delta_r = m.radians(30)                     # Max rudder deflection
     X_v = Q_("5.70 m")                                  # [m] 0.25C location compared to the nose
     Z_v = Q_("0.52 m")                                  # [m] Distance MAC_h and zero lift line wing
-
+    t_c = 0.15                                  # [-] t/c V-tail
     
 class Landing_gear(object):
     
