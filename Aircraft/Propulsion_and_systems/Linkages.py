e47105c44a17755aac97f3fa5706744b56dddf2c
Ties <ties.a.nieuwenhuizen@gmail.com>
2018-06-12 17:23:36 +0200

    Added more linkage force calculations

--- Aircraft/Propulsion_and_systems/Linkages.py
+++ Aircraft/Propulsion_and_systems/Linkages.py
@@ -43,29 +43,42 @@ def initialise_rudder_travel(inp):
 
 # Start assigning values to variables
 # Maximum stick force in roll
-f_s_r_max = Q_("20 lbf")  # From MIL-F-8785c
+f_r_max = Q_("20 lbf")  # From MIL-F-8785c
 
 # Maximum stick force in pitch
-f_s_p_max = f_s_r_max*2  # From 1:2:3 ratio for control forces
+f_p_max = f_r_max*2  # From 1:2:3 ratio for control forces
 
-# Maximum rudder force
-f_r_max = f_s_r_max*3  # From 1:2:3 ratio for control forces
+# Maximum yaw force on pedals
+f_y_max = f_r_max*3  # From 1:2:3 ratio for control forces
 
 # End assigning values
 
+# Hinge moments calculation
 
 # Calculate maximum hinge moment in roll
-m_s_r_max = f_s_r_max * Stick_pedals.l_s
-m_s_r_max.ito(ureg("newton * meter"))
-print(m_s_r_max)
+m_r_max = f_r_max * Stick_pedals.l_s
+m_r_max.ito(ureg("newton * meter"))
+print("Maximum roll hinge moment: {}".format(m_r_max))
 
 # Calculate maximum hinge moment in pitch
-m_s_p_max = f_s_p_max * Stick_pedals.l_s
-m_s_p_max.ito(ureg("newton * meter"))
-print(m_s_p_max)
+m_p_max = f_p_max * Stick_pedals.l_s
+m_p_max.ito(ureg("newton * meter"))
+print("Maximum pitch hinge moment: {}".format(m_p_max))
 
-# Calculate maximum hinge moment in yaw
+# Arm between rudder and cable/pushrod attachment
 r_rudder = Stick_pedals.d_p/np.tan(Geometry.V_tail.delta_r)
-m_r_max = f_r_max * r_rudder
-m_r_max.ito(ureg("newton * meter"))
-print(m_r_max)
+# Calculate maximum hinge moment in yaw
+m_y_max = f_y_max * r_rudder
+m_y_max.ito(ureg("newton * meter"))
+print("Maximum yaw hinge moment: {}".format(m_y_max))
+
+# Calculate forces in pushrods
+# Max roll force on stick below hinge
+f_r_b_max = (Stick_pedals.l_s/Stick_pedals.l_s_b) * f_r_max
+f_r_b_max.ito(ureg("newton"))
+print("Maximum roll force on stick below hinge: {}".format(f_r_b_max))
+
+# Max pitch force on stick below hinge
+f_p_b_max = (Stick_pedals.l_s/Stick_pedals.l_s_b) * f_p_max
+f_p_b_max.ito(ureg("newton"))
+print("Maximum pitch force on stick below hinge: {}".format(f_p_b_max))
