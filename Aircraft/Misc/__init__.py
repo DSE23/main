1454de27db410dcc66d8cb4ca09a03e322d98384
tobiashaegens <tobias.haegens@gmail.com>
2018-06-12 10:34:34 +0200

    deformation and error stuff

--- Aircraft/Misc/__init__.py
+++ Aircraft/Misc/__init__.py
@@ -4,6 +4,7 @@ DO NOT EDIT/DELETE
 SEE THE FOLLOWING LINK WHY THIS NEEDS TO BE HERE:
     https://pint.readthedocs.io/en/latest/tutorial.html
 """
-from pint import UnitRegistry
+from pint import UnitRegistry, set_application_registry
 ureg = UnitRegistry()
+set_application_registry(ureg)
 Q_ = ureg.Quantity
