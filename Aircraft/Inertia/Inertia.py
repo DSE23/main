aea6c785a4f083a64884a94200ef0d699566cbac
Jurian Stougie <jurianstougie@live.nl>
2018-06-13 13:46:19 +0200

    Added H-tail in mMoI

--- Aircraft/Inertia/Inertia.py
+++ Aircraft/Inertia/Inertia.py
@@ -33,21 +33,22 @@ Sparloc2 = strucwing.ChSpar2
 XLEMAC = Geometry.CG.XLEMAC
 Sweep_LE = Geometry.Wing.Sweep_LE
 Y_mac = (b/6)*((1+2*taper_w)/(1+taper_w))
+W_Htail = Geometry.Masses.W_htail
 W_wing = Geometry.Masses.W_wing
 W_spar1 = StrucVal.Weightspar1
 W_spar2 = StrucVal.Weightspar2
-rho_rib = Q_(" 1 kg/m**3")                                         # Density rib material
+rho_rib = StrucVal.Density                          # Density rib material
 k_rib = 0.5 * 10**-3                                # Value from Inertia report
 t_ref = Q_("1 m")                                   # Reference thickness
 t_c = Geometry.Wing.T_Cmax
 t_rootwrib = c_rw * t_c
-t_tipwrib =  c_tw * t_c
-W_rib = k_rib * rho_rib * S_wing *(t_ref + (t_rootwrib + t_tipwrib)/2)
+t_tipwrib = c_tw * t_c
+W_rib = k_rib * rho_rib * S_wing * (t_ref + (t_rootwrib + t_tipwrib)/2)
 W_skin = W_wing - (W_spar1 + W_spar2 + W_rib)
 F_fs = W_spar1/W_wing
 F_rs = W_spar2/W_wing
 F_skin = W_skin/W_wing
-F_ribs = 1 
+F_ribs = W_rib/W_wing
 
 # Fuselage
 Z_cg = Geometry.CG.Z_cg
@@ -167,11 +168,11 @@ x_c2 = 0.5 * (Sparloc1 + xms)
 x_c3 = 0.5 * (xms + Sparloc2)
 x_c4 = 0.5 * (1 + Sparloc2)
 x_c5 = 1.0
-L1 = (x_c1-x_c0)
-L2 = x_c2 - x_c1
-L3 = x_c3 - x_c2
-L4 = x_c4 - x_c3
-L5 = x_c5 - x_c4
+L1 = x_c1
+L2 = x_c2 
+L3 = x_c3 
+L4 = x_c4 
+L5 = 1
 xcgw = np.array([[(xapexw + L1/2 * chordw).magnitude],
                 [(xapexw + Sparloc1 * chordw).magnitude],
                 [(xapexw + xms * chordw).magnitude],
@@ -200,8 +201,9 @@ N_windex = np.linspace(1,40,40)
 mpmw = np.array([[((L1 * F_skin + A_airfoilfrac[0]*F_ribs)*(A1+par*(N_windex-1))).magnitude],
                  [(((L2 - L1) * F_skin + A_airfoilfrac[1] * F_ribs + F_fs)*(A1 + par*(N_windex-1))).magnitude],
                  [(((L3 - L2) * F_skin + A_airfoilfrac[2] * F_ribs)*(A1 + par*(N_windex-1))).magnitude],
-                 [(((L4 - L3) * F_skin + A_airfoilfrac[3] * F_ribs + F_rs)*(A1 + par*(N_windex-1))).magnitude]])
-
+                 [(((L4 - L3) * F_skin + A_airfoilfrac[3] * F_ribs + F_rs)*(A1 + par*(N_windex-1))).magnitude],
+                 [(((1 - L4) * F_skin + A_airfoilfrac[4] * F_ribs)*(A1 + par* (N_windex-1))).magnitude]])
+mpmw = mpmw[:, 0, 0, :] * Q_("kg")
 
 I_xxpmw = mpmw * ((ycgw - Y_cg)**2 + (zcgw - Z_cg)**2)
 I_yypmw = mpmw * ((zcgw - Z_cg)**2 + (xcgw - X_cg)**2)
@@ -211,3 +213,50 @@ I_xxw = 2 * sum(sum(I_xxpmw))
 I_yyw = 2 * sum(sum(I_yypmw))
 I_zzw = 2 * sum(sum(I_zzpmw))
 I_xyw = 2 * sum(sum(I_xypmw))
+
+
+# Horizontal Tail
+c_rh = Geometry.H_tail.c_r
+b_h = Geometry.H_tail.b
+c_th = Geometry.H_tail.c_t
+Sweep_LE = Geometry.H_tail.Sweep_LE
+C_ah = min(c_rh, ((b_h*np.tan(Sweep_LE))/2), (c_th + (b_h*np.tan(Sweep_LE))/2) )
+C_bh = b_h*np.tan(Sweep_LE)/2 + c_th
+C_ch = max(c_rh, ((b_h*np.tan(Sweep_LE))/2), (c_th + (b_h*np.tan(Sweep_LE))/2) )
+rho_h = W_Htail/ (0.5 * ( -C_ah + C_bh + C_ch))
+x_h = np.linspace(0, c_rh, 50)
+x_h1 = np.linspace(0, C_ah, 20)
+x_h2 = np.linspace(C_ah, C_bh, 20)
+x_h3 = np.linspace(C_bh, C_ch, 20)
+x_h1 *= Q_("m")
+hdx1 = x_h1[1]-x_h1[0]
+x_h2 *= Q_("m")
+hdx2 = x_h2[1]-x_h2[0]
+x_h3 *= Q_("m")
+hdx3 = x_h3[1]-x_h3[0]
+y1 = rho_h/C_ah * x_h1
+y2 = rho_h * (20*[1])
+y3 = -((rho_h* x_h3)/(C_ch - C_bh)) + (rho_h * C_ch)/(C_ch - C_bh)
+I_h = rho_h/12 * (-C_ah**3 + C_bh**3 + C_ch**2 * C_bh + C_ch*C_bh**2 + C_ch**3)
+K_0 = 0.771                             # From source 
+sigmx_h = sum(y1 * hdx1 * x_h1) + sum(y2 * hdx2 * x_h2) + sum(y3 * hdx3 * x_h3)
+sigm_h = sum(y1 * hdx1) + sum(y2 * hdx2) + sum(y3 * hdx3)
+I_0yh = K_0 * (I_h - sigmx_h**2/sigm_h)
+y_h = Q_("0.6 m")
+H_rollcoeff = y_h/(b_h/6*((c_rh + 2 * c_th)/(c_rh + c_th)))
+k_4 = 0.88                              # From graphs, dependent on H_rollcoeff
+I_0xh = (W_Htail * b_h**2 * k_4)/24 * ((c_rh+3*c_th)/(c_rh +c_th))
+I_0zh = I_0yh + I_0xh
+ycgh = 0
+zcgh = Geometry.H_tail.Z_h              # Symmetric wing, so cg in middle of height
+xcgh = Geometry.CG.CG_htail             # CG location in x-axis of the H-tail
+I_xxh = I_0xh + W_Htail * ((ycgh - Y_cg)**2 + (zcgh - Z_cg)**2)
+I_yyh = I_0yh + W_Htail * ((xcgh - X_cg)**2 + (zcgh - Z_cg)**2)
+I_zzh = I_0zh + W_Htail * ((xcgh - X_cg)**2 + (ycgh - Y_cg)**2)
+Ixz = W_Htail * (xcgh - X_cg)*(zcgh - Z_cg)
+
+# Vertical Tail
+c_rv = Geometry.V_tail.c_r
+b_v = Geometry.V_tail.b
+c_th = Geometry.V_tail.c_t
+Sweep_LEv = Geometry.V_tail.Sweep_LE
