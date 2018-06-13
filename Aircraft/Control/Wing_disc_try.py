51e0b922cd886835469bb95b1ed806a75c46b947
Jordy van Leeuwen <j.vanleeuwen10@gmail.com>
2018-06-13 17:33:21 +0200

    Imported aero data - but I think there is something not correct

--- Aircraft/Control/Wing_disc_try.py
+++ Aircraft/Control/Wing_disc_try.py
@@ -20,31 +20,64 @@ from Inertia import Inertia
 from Misc import Init_parm as IP
 import matplotlib.pyplot as plt
 import math
+import pandas as pd
 t0 = time.time()
 
 # Definitions
 def local_chord(z,c_r,c_t,half_b):
     # Calculates the chord at location z(distance from center)
     return c_r-(c_r-c_t)/half_b*z
-def lookup_cl(alpha,da):
-    dcl_da = da/38.
-    return cl_func(math.degrees(alpha)) + dcl_da
-
 
 # import airfoil lookup tables
-f = open("cl_alpha.txt")
-cl_alpha = f.readlines()
-f.close
-cl_alpha_lst = len(cl_alpha)*[0.]
-cl_lst = len(cl_alpha)*[0.]
-winglst = []
+data = pd.read_csv('aerodynamic_data_ms15.dat',' ',header=None).values
+def lookup_data(alpha,ca_c,da):
+    alpha = math.degrees(alpha)
+    da = round(da/5.,0)*5
+    ca_c = round(ca_c/0.01,0)*0.01
+    
+    data_over_da = np.zeros((7,3))
+    iterator = 0
+    for da_iterate in np.arange(0,31,5):
+        row_count_ca = int((ca_c-0.01)*100*51)
+        row_count_da = 2550*int(da_iterate/5)
+        local_data = data[range(0+row_count_ca+row_count_da,51+row_count_ca+row_count_da),:]
+        non_zero_max = max(np.argwhere(local_data[:,0]))[0] # last non-zero row
+        local_data = local_data[range(0,non_zero_max+1),:]
+        
+        if alpha> max(local_data[:,0]):
+            if (alpha-max(local_data[:,0]))<1:
+                alpha = max(local_data[:,0])
+            else:
+                raise ValueError("Alpha is out of range")
+        if abs(alpha) > max(local_data[:,0]) and alpha<0:
+            alpha = -max(local_data[:,0])
+        plt.plot(local_data[:,0],local_data[:,1])         
+        Cl_local = interpolate.interp1d(local_data[:,0],local_data[:,1],'linear')
+        Cd_local = interpolate.interp1d(local_data[:,0],local_data[:,2],'linear')
 
-for i in range (0,len(cl_alpha)):
-    cl_alpha[i] = cl_alpha[i].strip("\n")
-    variables  = cl_alpha[i].split("\t")
-    cl_alpha_lst[i] = float(variables[0])
-    cl_lst[i]       = float(variables[1])
-cl_func = interpolate.interp1d(cl_alpha_lst,cl_lst,'cubic')
+        if alpha>=0.:
+            Cl = Cl_local(alpha)
+            Cd = Cd_local(alpha)
+        else:
+            Cl = -Cl_local(-alpha)
+            Cd = Cd_local(-alpha)
+        
+        data_over_da[iterator,0] = da_iterate
+        data_over_da[iterator,1] = Cl
+        data_over_da[iterator,2] = Cd    
+        iterator +=1
+    
+    cl_func = interpolate.interp1d(data_over_da[:,0],data_over_da[:,1])
+    cd_func = interpolate.interp1d(data_over_da[:,0],data_over_da[:,2])
+    
+    if da>=0.:
+        Cl_final = cl_func(da)*1
+        Cd_final = cd_func(da)*1
+        #print (data_over_da)
+    else:
+        Cl_final = -cl_func(-da)*1
+        Cd_final = cd_func(-da)*1
+    return Cl_final, Cd_final
 
 # Import Aircraft Geometry
 I_yy  = Inertia.I_yy
@@ -103,7 +136,7 @@ for l in range(n_chords_h):
 for k in range(n_of_disc_v+1):
     kvlst[k]   = 0.312*ureg.m-b_v + bloc_v*k
 
-da    = 30.                     # aileron deflection
+da    = 25.                     # aileron deflection
 alpha_nose = 0.                 # angle of attack of nose
 beta_nose  = 0.                 # angle of sideslip of nose
 p     = 0./ureg.s               # initial roll rate
@@ -117,8 +150,8 @@ pmax = np.transpose(np.vstack([Vrange,np.zeros((1,len(Vrange)))[0]]))
 #    V_inf = V*ureg.m/ureg.s
 V_inf = 90*ureg.m/ureg.s
 t_current = Q_("0 s")
-dt = Q_("0.02 s")
-t_sim = Q_("0.7 s")
+dt = Q_("0.01 s")
+t_sim = Q_("0.1 s")
 running = True
 n = 0
 plst = np.zeros((1,int((t_sim/dt).magnitude+1)))[0]
@@ -126,7 +159,9 @@ pdlst= np.zeros((1,int((t_sim/dt).magnitude+1)))[0]
 tlst = np.arange(0,t_sim.magnitude+dt.magnitude,dt.magnitude)
 llst = []
 while running:
-
+    if (t_current.magnitude+dt.magnitude) > t_sim.magnitude:
+        running = False
+        
     disc_wing_w = np.zeros((len(kwlst)-1,6))    # 2D array discretized wing
     disc_wing_h = np.zeros((len(khlst)-1,6))    # 2D array discretized HT
     disc_wing_v = np.zeros((len(kvlst)-1,6))    # 2D array discretized VT
@@ -143,26 +178,26 @@ while running:
         c1 = local_chord(abs(b1),c_r_w,c_t_w,half_b_w)   # Chord left of piece
         c2 = local_chord(abs(b2),c_r_w,c_t_w,half_b_w)   # Chord right of piece
         cc = local_chord(abs(y_i),c_r_w,c_t_w,half_b_w)
-        ca_c = l_a/cc                   # percentage aileron chord over local aileron
+        ca_c = (l_a/cc).magnitude                   # percentage aileron chord over local aileron
         Sloc    = (c1+c2)/2*(b2-b1)     # Surface area of piece
         
         alpha_w   = alpha_nose + p*y_i/V_inf
         alpha_i   = 0.
         for j in range(10):
             alpha_e = alpha_w - alpha_i
-            Cl = lookup_cl(alpha_e,da_local)
+            Cl, Cd = lookup_data(alpha_e,ca_c,da_local)
             alpha_i = Cl/(math.pi*AR_w*e_w)         
         alpha = alpha_w - alpha_i
         downwash_angle = 2*Cl/(math.pi*AR_w)
-        
-        Lift  = 0.5 * rho * V_inf**2 * Sloc * lookup_cl(alpha,da_local)
-        Drag  = 0.5 * rho * V_inf**2 * Sloc * lookup_cl(alpha,da_local)
+        Cl, Cd = lookup_data(alpha,ca_c,da_local)
+        Lift  = 0.5 * rho * V_inf**2 * Sloc * Cl
+        Drag  = 0.5 * rho * V_inf**2 * Sloc * Cd
         disc_wing_w[i-1][1] = math.degrees(downwash_angle)
-        disc_wing_w[i-1][2] = Sloc.magnitude
+        disc_wing_w[i-1][2] = (p*y_i/V_inf).magnitude
         disc_wing_w[i-1][3] = Lift.magnitude
         disc_wing_w[i-1][4] = (Drag*y_i).magnitude  # Change to Cd!
         disc_wing_w[i-1][5] = (Lift*y_i).magnitude
-    
+    print (np.degrees(disc_wing_w[:,2]))
     for i in range(1,len(khlst)):
         # calculate lift and drag for discretized HT
         b1 = khlst[i-1]
@@ -171,9 +206,10 @@ while running:
         c1 = local_chord(abs(b1),c_r_h,c_t_h,half_b_h)
         c2 = local_chord(abs(b2),c_r_h,c_t_h,half_b_h)
         Sloc = (c1+c2)/2*(b2-b1)
-        alpha = alpha_nose + p*y_i/V_inf
-        Lift  = 0.5 * rho * V_inf**2 * Sloc * lookup_cl(alpha,0)*1
-        Drag  = 0.5 * rho * V_inf**2 * Sloc * lookup_cl(alpha,0)*1
+        alpha = alpha_nose #+ p*y_i/V_inf
+        Cl, Cd = lookup_data(alpha,0.01,0.)
+        Lift  = 0.5 * rho * V_inf**2 * Sloc * Cl
+        Drag  = 0.5 * rho * V_inf**2 * Sloc * Cd
         disc_wing_h[i-1][1] = Sloc.magnitude
         disc_wing_h[i-1][2] = y_i.magnitude
         disc_wing_h[i-1][3] = Lift.magnitude
@@ -188,9 +224,10 @@ while running:
         c1 = local_chord(abs(b1-0.312*ureg.m),c_r_v,c_t_v,b_v)
         c2 = local_chord(abs(b2-0.312*ureg.m),c_r_v,c_t_v,b_v)
         Sloc = (c1+c2)/2*(b2-b1)
-        beta = beta_nose + p*z_i/V_inf
-        Lift  = 0.5 * rho * V_inf**2 * Sloc * lookup_cl(beta,0)*1
-        Drag  = 0.5 * rho * V_inf**2 * Sloc * lookup_cl(beta,0)*1
+        beta = beta_nose  + p*z_i/V_inf
+        Cl, Cd = lookup_data(beta,0.01,0.)
+        Lift  = 0.5 * rho * V_inf**2 * Sloc * Cl
+        Drag  = 0.5 * rho * V_inf**2 * Sloc * Cd
         disc_wing_v[i-1][1] = Sloc.magnitude
         disc_wing_v[i-1][2] = beta.magnitude
         disc_wing_v[i-1][3] = Lift.magnitude
@@ -209,8 +246,7 @@ while running:
     llst.append(L)
     
     # terminate simulation at specified end time
-    if t_current.magnitude > t_sim.magnitude:
-        running = False
+
         
     t_current += dt
     n += 1
