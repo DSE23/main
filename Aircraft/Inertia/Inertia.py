cebd570f4b290eae8da1c3bdb77802851f567f0e
Jurian Stougie <jurianstougie@live.nl>
2018-06-13 16:25:16 +0200

    All the mMoI caculations(missing some locations)

--- Aircraft/Inertia/Inertia.py
+++ Aircraft/Inertia/Inertia.py
@@ -16,6 +16,7 @@ import numpy as np
 from Geometry import Geometry
 from Structures import Wing as strucwing
 from Structures import StrucVal
+from Propulsion_and_systems import Engine
 
 I_yy = Q_("1594 kg m**2")
 I_xx = Q_("1089 kg m**2")
@@ -34,7 +35,9 @@ XLEMAC = Geometry.CG.XLEMAC
 Sweep_LE = Geometry.Wing.Sweep_LE
 Y_mac = (b/6)*((1+2*taper_w)/(1+taper_w))
 W_Htail = Geometry.Masses.W_htail
+W_Vtail = Geometry.Masses.W_vtail
 W_wing = Geometry.Masses.W_wing
+W_wing = W_wing + Geometry.Masses.W_flaperons + Geometry.Masses.W_lehld
 W_spar1 = StrucVal.Weightspar1
 W_spar2 = StrucVal.Weightspar2
 rho_rib = StrucVal.Density                          # Density rib material
@@ -129,7 +132,7 @@ mpm = W_ifus * s_pm/(sum(s_pm))
 I_xxpmf = mpm * ((ycgf - Y_cg)**2 + (zcgf - Z_cg)**2)
 I_yypmf = mpm * ((zcgf - Z_cg)**2 + (xcgf - X_cg)**2)
 I_zzpmf = mpm * ((xcgf - X_cg)**2 + (ycgf - Y_cg)**2)
-I_xzpmf = mpm * ((xcgf - X_cg)+(zcgf - Z_cg))
+I_xzpmf = mpm * ((xcgf - X_cg) * (zcgf - Z_cg))
 I_xxf = np.sum(np.sum(I_xxpmf))
 I_yyf = np.sum(np.sum(I_yypmf))
 I_zzf = np.sum(np.sum(I_zzpmf))
@@ -181,7 +184,7 @@ xcgw = np.array([[(xapexw + L1/2 * chordw).magnitude],
 xcgw = xcgw[:, 0, :] * Q_("m")
 ycgw = np.tile(ycgw, (5,1))
 ycgw = ycgw * Q_("m")
-zcgw = np.zeros(N_stw)
+zcgw = np.zeros(N_stw) * Q_("m")
 a = -W_wing*(((c_rw*(1-taper_w))/sum(chordw))/N_stw)**2
 C1 = 2/b.magnitude *(W_wing.magnitude/2 - b.magnitude**2/8 * a.magnitude)
 C1 = C1 * Q_("m * kg")
@@ -208,11 +211,11 @@ mpmw = mpmw[:, 0, 0, :] * Q_("kg")
 I_xxpmw = mpmw * ((ycgw - Y_cg)**2 + (zcgw - Z_cg)**2)
 I_yypmw = mpmw * ((zcgw - Z_cg)**2 + (xcgw - X_cg)**2)
 I_zzpmw = mpmw * ((xcgw - X_cg)**2 + (ycgw - Y_cg)**2)
-I_xypmw = mpmw * ((xcgw - X_cg) + (zcgw - Z_cg))
+I_xzpmw = mpmw * ((xcgw - X_cg) * (zcgw - Z_cg))
 I_xxw = 2 * sum(sum(I_xxpmw))
 I_yyw = 2 * sum(sum(I_yypmw))
 I_zzw = 2 * sum(sum(I_zzpmw))
-I_xyw = 2 * sum(sum(I_xypmw))
+I_xzw = 2 * sum(sum(I_xzpmw))
 
 
 # Horizontal Tail
@@ -224,10 +227,10 @@ C_ah = min(c_rh, ((b_h*np.tan(Sweep_LE))/2), (c_th + (b_h*np.tan(Sweep_LE))/2) )
 C_bh = b_h*np.tan(Sweep_LE)/2 + c_th
 C_ch = max(c_rh, ((b_h*np.tan(Sweep_LE))/2), (c_th + (b_h*np.tan(Sweep_LE))/2) )
 rho_h = W_Htail/ (0.5 * ( -C_ah + C_bh + C_ch))
-x_h = np.linspace(0, c_rh, 50)
-x_h1 = np.linspace(0, C_ah, 20)
-x_h2 = np.linspace(C_ah, C_bh, 20)
-x_h3 = np.linspace(C_bh, C_ch, 20)
+n_disctail = 25
+x_h1 = np.linspace(0, C_ah, n_disctail)
+x_h2 = np.linspace(C_ah, C_bh, n_disctail)
+x_h3 = np.linspace(C_bh, C_ch, n_disctail)
 x_h1 *= Q_("m")
 hdx1 = x_h1[1]-x_h1[0]
 x_h2 *= Q_("m")
@@ -235,15 +238,19 @@ hdx2 = x_h2[1]-x_h2[0]
 x_h3 *= Q_("m")
 hdx3 = x_h3[1]-x_h3[0]
 y1 = rho_h/C_ah * x_h1
-y2 = rho_h * (20*[1])
+y2 = rho_h * (n_disctail*[1])
 y3 = -((rho_h* x_h3)/(C_ch - C_bh)) + (rho_h * C_ch)/(C_ch - C_bh)
 I_h = rho_h/12 * (-C_ah**3 + C_bh**3 + C_ch**2 * C_bh + C_ch*C_bh**2 + C_ch**3)
 K_0 = 0.771                             # From source 
 sigmx_h = sum(y1 * hdx1 * x_h1) + sum(y2 * hdx2 * x_h2) + sum(y3 * hdx3 * x_h3)
 sigm_h = sum(y1 * hdx1) + sum(y2 * hdx2) + sum(y3 * hdx3)
 I_0yh = K_0 * (I_h - sigmx_h**2/sigm_h)
-y_h = Q_("0.6 m")
+dxLE_h =  0.25 * c_rh - 0.25* c_th 
+y_h = (2*c_th* dxLE_h + c_th**2 + dxLE_h * c_rh +\
+       c_th * c_rh + c_rh**2)/(3*(c_rh + c_th)) 
 H_rollcoeff = y_h/(b_h/6*((c_rh + 2 * c_th)/(c_rh + c_th)))
+if not 0.95 < H_rollcoeff < 0.97:
+    print(" !!!!Change k_4!!!!!")
 k_4 = 0.88                              # From graphs, dependent on H_rollcoeff
 I_0xh = (W_Htail * b_h**2 * k_4)/24 * ((c_rh+3*c_th)/(c_rh +c_th))
 I_0zh = I_0yh + I_0xh
@@ -253,10 +260,136 @@ xcgh = Geometry.CG.CG_htail             # CG location in x-axis of the H-tail
 I_xxh = I_0xh + W_Htail * ((ycgh - Y_cg)**2 + (zcgh - Z_cg)**2)
 I_yyh = I_0yh + W_Htail * ((xcgh - X_cg)**2 + (zcgh - Z_cg)**2)
 I_zzh = I_0zh + W_Htail * ((xcgh - X_cg)**2 + (ycgh - Y_cg)**2)
-Ixz = W_Htail * (xcgh - X_cg)*(zcgh - Z_cg)
+I_xzh = W_Htail * (xcgh - X_cg)*(zcgh - Z_cg)
 
 # Vertical Tail
 c_rv = Geometry.V_tail.c_r
 b_v = Geometry.V_tail.b
-c_th = Geometry.V_tail.c_t
+c_tv = Geometry.V_tail.c_t
 Sweep_LEv = Geometry.V_tail.Sweep_LE
+C_av = min(c_rv, ((b_v*np.tan(Sweep_LEv))/2), (c_tv + (b_v*np.tan(Sweep_LEv))/2))
+C_bv = c_tv + (b_v*np.tan(Sweep_LEv))/2
+C_cv = max(c_rv, ((b_v*np.tan(Sweep_LEv))/2), (c_tv + (b_v*np.tan(Sweep_LEv))/2))
+rho_v =W_Vtail/(0.5 * (-C_av + C_bv + C_cv))
+x_v1 = np.linspace(0, C_av, n_disctail)
+x_v2 = np.linspace(C_av, C_bv, n_disctail)
+x_v3 = np.linspace(C_bv, C_cv, n_disctail)
+x_v1 *= Q_("m")
+vdx1 = x_v1[1] - x_v1[0]
+x_v2 *= Q_("m")
+vdx2 = x_v2[1] - x_v2[0]
+x_v3 *= Q_("m")
+vdx3 = x_v3[1] - x_v3[0]
+yv1 = rho_v/C_av * x_v1
+yv2 = rho_v * (n_disctail*[1])
+yv3 = -((rho_v * x_v3)/(C_cv - C_bv)) + (rho_v * C_cv)/(C_cv - C_bv)
+I_v = rho_v/12 * (-C_av**3 + C_bv**3 + C_cv**2 * C_bv + C_cv * C_bv**2 + C_cv**3)
+dxLE_v = 0.25 * c_rv - 0.25*c_th
+z_vbar = (2*c_tv* dxLE_v + c_tv**2 + dxLE_v * c_rv +\
+       c_tv * c_rv + c_rv**2)/(3*(c_rv + c_tv)) 
+V_rollcoef = z_vbar/((b_v/3)*(c_rv + 2* c_tv)/(c_rv + c_tv))
+k_5 = 1.4
+if not 1.6 < V_rollcoef < 1.65:
+    print(" !!!! Change K_5!!!!!")
+I_0xv = (W_Vtail * b_v**2 * k_5)/18 * (1+ (2*c_rv*c_tv)/(c_rv+c_tv)**2)
+sigmx_v = sum(yv1 * vdx1 * x_v1) + sum(yv2 * vdx2 * x_v2) + sum(yv3 * vdx3 * x_v3)
+sigm_v = sum(yv1 * vdx1) + sum(yv2 * vdx2) + sum(yv3 * vdx3)
+I_0zv = K_0 * (I_v - sigmx_v**2/sigm_v)
+I_0yv = I_0zv + I_0xv
+ycgv = 0
+zcgv = z_vbar
+xcgv = Geometry.CG.CG_vtail
+I_xxv = I_0xv + W_Vtail * ((ycgv - Y_cg)**2 + (zcgv - Z_cg)**2)
+I_yyv = I_0yv + W_Vtail * ((xcgv - X_cg)**2 + (zcgv - Z_cg)**2)
+I_zzv = I_0zv + W_Vtail * ((xcgv - X_cg)**2 + (ycgv - Y_cg)**2)
+I_xzv = W_Vtail * (xcgv - X_cg)*(zcgv - Z_cg)
+
+# Engine
+
+W_engine = Engine.mass
+I_0xe = Engine.ixg
+I_0ye = Engine.iyg
+I_0ze = Engine.izg
+zcge = Q_("0 m")
+ycge = Q_("0 m")
+xcge = Geometry.CG.CG_engine
+I_xxe = I_0xe + W_engine * ((ycge - Y_cg)**2 + (zcge - Z_cg)**2)
+I_yye = I_0ye + W_engine * ((xcge - X_cg)**2 + (zcge - Z_cg)**2)
+I_zze = I_0ze + W_engine * ((xcge - X_cg)**2 + (ycge - Y_cg)**2)
+I_xze = W_engine *(xcge - X_cg) * (zcge - Z_cg)
+
+
+# Fuel
+
+W_fuel = Geometry.Masses.W_fuel
+xcgfuel = Geometry.CG.CG_fuel
+zcgfuel = Q_("0 m")
+ycgfuel = Q_("0 m")
+I_xxfuel = W_fuel * ((ycgfuel - Y_cg)**2 + (zcgfuel - Z_cg)**2)
+I_yyfuel = W_fuel * ((xcgfuel - X_cg)**2 + (zcgfuel - Z_cg)**2)
+I_zzfuel = W_fuel * ((xcgfuel - X_cg)**2 + (ycgfuel - Y_cg)**2)
+I_xzfuel = W_fuel * (xcgfuel - X_cg) * (zcgfuel - Z_cg)
+
+# Pilot
+
+W_pilot = Geometry.Masses.W_pilot
+xcgp = Geometry.CG.CG_pilot
+zcgp = Q_("0 m")
+ycgp = Q_("0 m")
+I_xxp = W_pilot * ((ycgp - Y_cg)**2 + (zcgp - Z_cg)**2)
+I_yyp = W_pilot * ((xcgp - X_cg)**2 + (zcgp - Z_cg)**2)
+I_zzp = W_pilot * ((xcgp - X_cg)**2 + (ycgp - Y_cg)**2)
+I_xzp = W_pilot * (xcgp - X_cg) * (zcgp - Z_cg)
+
+# Landing Gear
+
+W_lg = Geometry.Masses.W_gear
+xcglg = Geometry.CG.CG_lgear
+ycglg = Q_("0 m")
+zcglg = Q_("0.5 m")
+I_xxlg = W_lg * ((ycglg - Y_cg)**2 + (zcglg - Z_cg)**2)
+I_yylg = W_lg * ((xcglg - X_cg)**2 + (zcglg - Z_cg)**2)
+I_zzlg = W_lg * ((xcglg - X_cg)**2 + (ycglg - Y_cg)**2)
+I_xzlg = W_lg * (xcglg - X_cg) * (zcglg - Z_cg)
+
+# Electronic Systems
+
+W_elec = Geometry.Masses.W_elecsys
+xcgel = Geometry.CG.CG_elecsys
+zcgel = Q_("0 m")
+ycgel = Q_("0 m")
+I_xxel = W_elec * ((ycgel - Y_cg)**2 + (zcgel - Z_cg)**2)
+I_yyel = W_elec * ((xcgel - X_cg)**2 + (zcgel - Z_cg)**2)
+I_zzel = W_elec * ((xcgel - X_cg)**2 + (ycgel - Y_cg)**2)
+I_xzel = W_elec * (xcgel - X_cg) * (zcgel - Z_cg)
+
+# Flight Controls
+
+W_fcon = Geometry.Masses.W_flightcontrol
+xcgc = Geometry.CG.CG_flightcon
+zcgc = Q_("0 m")
+ycgc = Q_("0 m")
+I_xxc = W_fcon * ((ycgc - Y_cg)**2 + (zcgc - Z_cg)**2)
+I_yyc = W_fcon * ((xcgc - X_cg)**2 + (zcgc - Z_cg)**2)
+I_zzc = W_fcon * ((xcgc - X_cg)**2 + (ycgc - Y_cg)**2)
+I_xzc = W_fcon * (xcgc - X_cg) * (zcgc - Z_cg)
+
+
+# Total new Inertia
+
+I_xxnew = I_xxf + I_xxw + I_xxv + I_xxh + I_xxe + I_xxfuel + I_xxp + I_xxlg +\
+          I_xxel + I_xxc
+I_yynew = I_yyf + I_yyw + I_yyv + I_yyh + I_yye + I_yyfuel + I_yyp + I_yylg +\
+          I_yyel + I_yyc
+I_zznew = I_zzf + I_zzw + I_zzv + I_zzh + I_zze + I_zzfuel + I_zzp + I_zzlg +\
+          I_zzel + I_zzc
+I_xznew = I_xzf + I_xzw + I_xzv + I_xzh + I_xze + I_xzfuel + I_xzp + I_xzlg +\
+          I_xzel + I_xzc
+print(I_xxnew, "instead of", I_xx)
+print(I_yynew, "instead of", I_yy)
+print(I_zznew, "instead of", I_zz)
+print(I_xznew, "instead of", 0)
+I_xx = I_xxnew
+I_yy = I_yynew
+I_zz = I_zznew
+I_xz = I_xznew
