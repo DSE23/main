efc7099dee9ecfab44414d7f2ce675ac155f6509
Jurian Stougie <jurianstougie@live.nl>
2018-06-17 22:34:56 +0200

    #Pirouette sim 2018

--- Aircraft/Control/Taxi_sim.py
+++ Aircraft/Control/Taxi_sim.py
@@ -16,6 +16,7 @@ import numpy as np
 from Geometry import Geometry
 from Aerodynamics import Aeroprops
 from Performance import Performance
+from Inertia import Inertia
 
 # First check CG position for tipping:
 
@@ -64,43 +65,68 @@ XHtail_25c = Geometry.H_tail.X_h
 L_tot = L_wing + L_htail
 N_force = W_mtow  - L_tot
 M_lift = - L_wing * (XWing_25c - X_cgmtow) - L_htail * (XHtail_25c - X_cgmtow)
-N_main = -N_force * (X_taillg - X_cgmtow)/Wheel_base - M_lift/Wheel_base
+N_main = (-N_force * (X_taillg - X_cgmtow)/Wheel_base - M_lift/Wheel_base)/2
 N_tail = N_force * (X_mainlg - X_cgmtow)/Wheel_base + M_lift/Wheel_base
 Max_allow_p = Q_("35 psi") / g0
 Max_allow_p.ito("kg/cm**2")
 print(Max_allow_p)
 print(Stat_load_main / g0)
 print(Stat_load_tail / g0)
-t_end = Q_("10 s")
+Max_load = Q_("270 lbs")
+Max_load.ito(Q_("kg"))
+print(Max_load)
+t_end = Q_("30 s")
 dt = Q_("0.01 s")
 Psy = Q_("0 deg")                             # Initial heading
 Y = 0                                       # Initial lateral position
 X = 0                                       # Initial X-position
-delta_w = Q_("0 deg")                       # Initial Wheel deflection
+delta_tail = Q_("0 deg")                       # Initial Wheel deflection
+delta_taildot = Q_("15 deg/s")
 Psilist =[]
 Psidotlist =[]
 Xlist = []
 Ylist = []
 tlist =[]
+I_zz = Inertia.I_zz
+heading = 0
+r = 0
+
+
+# Cornering stiffness
+r_tail = Q_("16 inch")
+r_tail.ito(Q_("m"))
+a_tail = 60
+w_tail = Q_("225 mm")
+w_tail.ito(Q_("m"))
+E_tail = Q_("27 *10 **6 N/m**2")
+s_tail = 0.15
+C_cc = 0.05
+C_roll = 0.04
+C_ctail = C_cc * N_tail
+C_cmain = C_cc * N_main
+C_dtail = C_roll* N_tail
+C_dmain = C_roll * N_main
 for t in np.arange(0, t_end.magnitude + dt.magnitude, dt.magnitude):
-    if t >= 8:
-        delta_w = Q_("0 deg")
-    elif t >= 2:
-        delta_w = Q_("25 deg")
-    Psydot = V_taxi/Wheel_base * np.tan(delta_w)    
-    Ydot = V_taxi * np.sin(Psy)
-    Xdot = V_taxi * np.cos(Psy)
-    Psy = Psy + Psydot*dt
-    Y = Y + Ydot*dt
-    X = X + Xdot*dt
-    Psilist.append(Psy.magnitude)
-    Psidotlist.append(Psydot)
+    if delta_tail < Q_("45 deg"):
+        delta_tail = delta_tail + delta_taildot * dt
+    delta_w = delta_tail - (np.tan((r *(X_taillg - X_cgmtow).magnitude)/V_taxi.magnitude))
+    F_ytail = (C_ctail * np.radians(delta_w)) * np.cos(delta_w) + C_ctail * np.sin(delta_w)
+    #F_yrudder = 1
+    M_yaw = F_ytail * (X_taillg - X_cgmtow)
+    rdot = M_yaw / I_zz
+    r = r + (rdot*dt).magnitude
+    heading = heading + r*dt.magnitude
+    V_x = np.cos(heading) * V_taxi
+    V_y = np.sin(heading) * V_taxi
+    X = X + V_x * dt
+    Y = Y + V_y * dt
     Xlist.append(X.magnitude)
     Ylist.append(Y.magnitude)
     tlist.append(t)
-plt.plot(tlist, Psilist)
-print(min(Psidotlist))
+plt.plot(Xlist, Ylist)
 plt.show()
+    
+
 
     
     
