6d8ceff3387884a2fc3d1821222d351639f2991a
midasgossye <midasgossye@hotmail.com>
2018-06-19 10:05:07 +0200

    Added new shear file

---
+++ Aircraft/Structures/Shear.py
@@ -0,0 +1,9 @@
+"""
+Name: Shear
+Department: Structures
+Last updated: 19/06/2018 10:04 by Midas
+"""
+
+import sys
+sys.path.append('../') # This makes sure the parent directory gets added to the system path
+from Misc import ureg, Q_ # Imports the unit registry from the Misc folder
