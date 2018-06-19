0b8ceff6e5b3dcb17b402bd643024ee5ec6392dd
tobiashaegens <tobias.haegens@gmail.com>
2018-06-19 10:45:23 +0200

    shearcenter, torque and twist added

--- Aircraft/Structures/Shear.py
+++ Aircraft/Structures/Shear.py
@@ -7,7 +7,8 @@ Last updated: 19/06/2018 10:04 by Midas
 import sys
 sys.path.append('../') # This makes sure the parent directory gets added to the system path
 from Misc import ureg, Q_ # Imports the unit registry from the Misc folder
-
+from Structures import WingStress
+from Structures import Wing
 # Calculate Boom Area's
 
 # Calculate base shear flow for every section of the wing box
@@ -19,9 +20,22 @@ from Misc import ureg, Q_ # Imports the unit registry from the Misc folder
 # Compute moments around a.c. caused by shear forces due to shear flows
 
 # Calculate shear center location
+def Shear_center(moment_shear,):
+    shear_center = moment_shear/WingStress.L
+    return shear_center
 
 # Calculate Torque
+def Torque_for_twist(shear_center):
+    T = WingStress.M + WingStress.L * shear_center
+    return T
 
 # Calculate Rate of Twist
-
+def Rate_of_twist(T):
+    constant = T/(4*Wing.Area_cell()*WingStress.shear_modulus)
+    integral = Wing.HSpar1/Wing.ThSpar1
+    integral += 2*Wing.length_Skin_x_c/Wing.ThSkin
+    integral += Wing.HSpar2/Wing.ThSpar2
+    dthetadz = constant/integral
+    return dthetadz
+    
 # Calculate shear stress
