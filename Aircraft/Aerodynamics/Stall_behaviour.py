fd135d527c8ecc374187526ed63a0321e2982066
Emma <emmademeester@hotmail.com>
2018-06-24 13:23:09 +0200

    DATA stall behaviour control

--- Aircraft/Aerodynamics/Stall_behaviour.py
+++ Aircraft/Aerodynamics/Stall_behaviour.py
@@ -17,18 +17,60 @@ from pint import UnitRegistry
 import Aeroprops
 from Propulsion_and_systems import Propeller
 
-axial_v = np.genfromtxt('../DataWalong')
+
+#import propeller data
+axial_v = np.genfromtxt('../DataWalong.txt')
 wlist = np.array([])
-for line in len(axial_v[1,:]):
+range_for = len(axial_v[1,:])
+for line in range(0,range_for):
     w = np.average(axial_v[line,:])
-    wlist = np.append(alist, w)
+    wlist = np.append(wlist, w)
+    
+
+#Aircraft Geometry imports
+width_fus = Geometry.Fuselage.D_fus_max.magnitude / 2
+LE_wing = 1
+root_c = Geometry.Wing.c_r.magnitude
+aileron_c = Geometry.Wing.c_a.magnitude
+LE_ht = Geometry.H_tail.X_h.magnitude
+root_c_ht = Geometry.H_tail.c_r.magnitude
+elevator_c_ratio = Geometry.H_tail.ce_c.magnitude
+taper_ht = Geometry.H_tail.taper
+span_ht = Geometry.H_tail.b.magnitude
+LE_vt = Geometry.V_tail.X_v.magnitude
+root_c_vt = Geometry.V_tail.c_r.magnitude
+taper_vt = Geometry.V_tail.taper
+rudder_c_ratio = Geometry.V_tail.cr_c.magnitude
+span_vt = Geometry.V_tail.b.magnitude
 #Contracted slipstream diameter
 V0 = 30                                                                 #free stream velocity
 Dia = Geometry.Prop.Diameter.magnitude                                  #diameter of the prop
 Radius_p = Dia / 2
 #ask Gijs which a to select for which velocity
-a = w / V0
+a = wlist[1] / V0
 l = Geometry.Fuselage.l_f
 for x in np.linspace(0,l,200):
     Radius_tube = Radius_p * m.sqrt((1 + a) / (1 + a * (1 + (x/(m.sqrt(Radius_p ** 2 + x **2))))))
     velocity = a*V0 + V0
+    print(Radius_tube)
+    
+    if LE_wing <= x <= LE_wing + 0.03:
+        area_ail = (Radius_tube - width_fus) * aileron_c * 2
+        velocity_ail = velocity
+        
+    elif LE_ht <= x <= LE_ht + 0.03:
+        lam_local = taper_ht + (1 - taper_ht)/span_ht * 2 * (span_ht/2 - Radius_tube)
+        chord_l = lam_local * root_c_ht * elevator_c_ratio
+        area_elevator = 2 * (chord_l + root_c_ht * elevator_c_ratio) * Radius_tube / 2
+        velocity_ele = velocity
+        
+    elif LE_vt <= x <= LE_vt + 0.03:
+        lam_local = taper_vt + (1 - taper_vt)/span_vt * (span_vt - Radius_tube)
+        chord_l = lam_local * root_c_vt * rudder_c_ratio
+        area_rudder = (chord_l + root_c_vt * rudder_c_ratio) * Radius_tube / 2
+        velocity_rud = velocity
+       
+
+
+
+
